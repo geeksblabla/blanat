69aeaf86ba09cf97c883313058ce3101b990ef33
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2024-02-17 11:27:41 +0000

    a bit faster, under 7 min
    Signed-off-by: Mohammed Aboullaite <aboullaite.mohammed@gmail.com>

--- submissions/aboullaite/Main.java
+++ submissions/aboullaite/Main.java
@@ -1,74 +1,59 @@
+
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.AbstractMap;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 // Naive implementation
 public class Main {
 
-    private record CityData(String name, String product, double price) {}
+  public static final String INPUT_TXT = "input.txt";
 
-    public static void main(String[] args) throws IOException {
-        Map<String, Double> cheapestCity = new HashMap<>();
-        Map<String, CityData[]> productsByCity = new HashMap<>();
-        // Maybe split this one into chucnks
-        try (Stream<String> lines = java.nio.file.Files.lines(Paths.get("input.txt"))) {
-            lines.forEach(line -> {
-                String[] content = line.split(",");
-                CityData data = new CityData(content[0], content[1], Double.parseDouble(content[2]));
-                // Lot of unboxing here! Optimize ?
-                cheapestCity.computeIfPresent(data.name(), (k,v) -> v + data.price());
-                cheapestCity.putIfAbsent(data.name(), data.price());
-                keepTrackof5CheapestProducts(data, productsByCity);
-            });
-        }
-        String city = Collections.min(cheapestCity.entrySet(),
-            Entry.comparingByValue()).getKey();
-
-        // TODO save in a file
-        System.out.println(city + " " + String.format("%.2f",cheapestCity.get(city)));
-        for (CityData d: productsByCity.get(city))
-        {
-            System.out.println(d.product +" "+ String.format("%.2f", d.price));
-        };
+  private record Product(String product, double price) {}
+  private record CityRecord(double total, Product[] products) {
+    CityRecord(double price, Product product) {
+      this(price, new Product[]{product});
     }
+    public static CityRecord combineWith(CityRecord c1, CityRecord c2) {
+      return new CityRecord(
+          c1.total() + c2.total(),
+          Stream.concat(Arrays.stream(c1.products()), Arrays.stream(c2.products))
+              .sorted(Comparator.comparingDouble(Product::price).thenComparing(Product::product))
+              .limit(5)
+              .toArray(Product[]::new)
+      );
+    }
+  }
 
-    // Keeping only track of 5 cheapest product by cities. 
-    // Using array as a stack here. Sorting every time might be expensive
-    private static void keepTrackof5CheapestProducts(CityData cityData,
-        Map<String, CityData[]> cache){
-        String key = cityData.name();
-        if(cache.containsKey(key)){
-            CityData data[] = cache.get(key);
-            if(data.length < 5){
-                data = Arrays.copyOf(data, data.length + 1);
-                data[cache.get(key).length] = cityData;
-            }else {
-                boolean exist = Arrays.stream(data).anyMatch(d -> d.name().equals(cityData.name()));
-                if(!exist){
-                    if(data[5].price() > cityData.price() ){
-                        data[5] = cityData;
-                    }
-                }else{
-                    for (int i=0; i< data.length; i++){
-                        if(data[i].name().equals(cityData.name()) && data[i].price() > cityData.price()){
-                            data[i] = cityData;
-                            break;
-                        }
-                    }
-                }
+  public static void main(String[] args) throws IOException {
+
+    try (Stream<String> lines = java.nio.file.Files.lines(Paths.get(INPUT_TXT))) {
+      Map<String, CityRecord> resultMap = lines.parallel().map(line -> {
+        String[] content = line.split(",");
+        Product product = new Product(content[1], Double.parseDouble(content[2]));
+        return new AbstractMap.SimpleEntry<>(content[0], product);
+      }).collect(Collectors.toConcurrentMap(
+          // Combine/reduce:
+          AbstractMap.SimpleEntry::getKey,
+          entry -> new CityRecord(entry.getValue().price(), entry.getValue()),
+              CityRecord::combineWith));
+
+      resultMap.entrySet().stream()
+          .sorted(Entry.comparingByValue(Comparator.comparingDouble(CityRecord::total)))
+          .limit(1)
+          .forEach(entry -> {
+            Product[] products = entry.getValue().products();
+            System.out.println(entry.getKey() + " " + String.format( "%.2f", entry.getValue().total()));
+            for (Product product : products) {
+              System.out.println(product.product() + " " + String.format( "%.2f", product.price()));
             }
-            Arrays.sort(data, Comparator.comparingDouble(CityData::price));
-            cache.put(key, data);
-        }else{
-            CityData data[] = {cityData};
-            cache.put(key,  data);
-        }
+          });
     }
+  }
 }
