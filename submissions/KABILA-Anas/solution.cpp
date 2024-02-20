a003c6c397d2550fd4d29786e624279c848fd0fe
Anas KBL <kabila.anas1@gmail.com>
2024-02-20 23:15:49 +0100

    blanat 1.0

--- submissions/KABILA-Anas/solution.cpp
+++ submissions/KABILA-Anas/solution.cpp
@@ -1,11 +1,27 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <memory>
 #include <cstring>
+#include <iomanip>
+#include <unordered_map>
+#include <queue>
+
 using namespace std;
 
+struct Product {
+    string name;
+    double price;
 
+    // Custom sorting
+    bool operator<(const Product& p) const {
+        return price == p.price ? name > p.name : price > p.price;
+    }
+};
+
+unordered_map<string, double> cities_prices;
+unordered_map<string, double> products_prices;
 
 void process_chunk(const string &chunk)
 {
@@ -20,7 +36,11 @@ void process_chunk(const string &chunk)
             getline(iss, city, ',');
             getline(iss, product, ',');
             iss >> price;
-            cout << city << " " << product << " " << price << std::endl;
+
+            cities_prices[city] += price;
+
+            if (products_prices.find(product) == products_prices.end() || products_prices[product] > price)
+                products_prices[product] = price;
         }
     }
 
@@ -42,11 +62,42 @@ void blanat()
     }
 
     file.close();
+
+    auto cheapest_city = min_element(cities_prices.begin(), cities_prices.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second;
+    });
+
+    string cheapestCity = cheapest_city->first;
+    double cheapestCityTotalPrice = cheapest_city->second;
+
+    priority_queue<Product> cheapestProducts;
+
+    // Add products of cheapest city to the priority queue
+    for (const auto& [product, price] : products_prices) {
+        cheapestProducts.push({product, price});
+    }
+
+    ofstream output("output.txt");
+    if (!output.is_open()) {
+        cerr << "Error opening file: " << strerror(errno) << endl;
+        return;
+    }
+
+    output << cheapestCity << " " << fixed << setprecision(2) << cheapestCityTotalPrice << endl;
+
+    int count = 0;
+    while (count < 5 && !cheapestProducts.empty()) {
+        output << cheapestProducts.top().name << " " << fixed << setprecision(2) << cheapestProducts.top().price << endl;
+        cheapestProducts.pop();
+        count++;
+    }
+
+    output.close();
+
 }
 
 int main()
 {
-    cout << "Hello, World!" << endl;
     blanat();
     return 0;
 }
