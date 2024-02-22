a04b494f62cd5f7edf50879e73194fdbcac019b3
Ouail Laamiri <116443517+LaamiriOuail@users.noreply.github.com>
2024-02-22 19:54:23 +0100

    blanate

--- submissions/ouail-laamiri/solution.py
+++ submissions/ouail-laamiri/solution.py
@@ -1,8 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
-import time
+# import time
 
-start_time = time.time()
+# start_time = time.time()
 
 spark = SparkSession.builder \
     .appName("Total price of products by city") \
@@ -15,7 +15,7 @@ schema = StructType([
 ])
 
 # Read the CSV-like file into a DataFrame
-df = spark.read.csv("./input.txt", schema=schema)
+df = spark.read.csv("../../input.txt", schema=schema)
 
 # Group by city and compute the total price of all products
 total_price_by_city = df.groupBy("city").sum("price")
@@ -23,22 +23,30 @@ total_price_by_city = df.groupBy("city").sum("price")
 # Find the city with the lowest total price
 city_with_lowest_price_row = total_price_by_city.orderBy("sum(price)").first()
 city_with_lowest_price = city_with_lowest_price_row["city"]
-total_price_in_lowest_city = city_with_lowest_price_row["sum(price)"]
+total_price_in_lowest_city = round(city_with_lowest_price_row["sum(price)"], 2)
 
 # Filter the DataFrame to include only records for the city with the lowest total price
 filtered_df = df.filter(df["city"] == city_with_lowest_price)
 
-# Order the filtered DataFrame by price in ascending order and select the first five rows
-five_least_expensive_products = filtered_df.orderBy("price").limit(5)
+
+# Group by product and compute the minimum price
+min_price_df = filtered_df.groupBy("product").agg({"price": "min"})
+
+# Order the DataFrame by price in ascending order and select the first five rows
+five_least_expensive_products = (
+    min_price_df.orderBy("min(price)", "product")
+    .limit(5)
+    .collect()
+)
 
 # Save the output to the file
 with open("./output.txt", "w") as file:
     file.write(f"{city_with_lowest_price} {total_price_in_lowest_city}\n")
-    for row in five_least_expensive_products.collect():
-        file.write(f"{row['product']} {row['price']}\n")
+    for row in five_least_expensive_products:
+        file.write(f"{row['product']} {round(row['min(price)'], 2)}\n")
 
-end_time = time.time()
-elapsed_time = end_time - start_time
-print("Elapsed time:", elapsed_time * 1000, "ms")
+# end_time = time.time()
+# elapsed_time = end_time - start_time
+# print("Elapsed time:", elapsed_time *1000, "ms")
 
 spark.stop()
