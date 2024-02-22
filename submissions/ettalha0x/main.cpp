cec21aa2e35fcf34c1fd52bac8fed20c0113dc72
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2024-02-23 00:42:29 +0100

    update

--- submissions/ettalha0x/main.cpp
+++ submissions/ettalha0x/main.cpp
@@ -1,55 +1,9 @@
-// #include <iostream>
-// #include <fstream>
-// #include <sstream>
-// #include <string>
-// #include <algorithm>
-// #include <map>
-// #include <vector>
-
-// using namespace std;
-
-// int main() {
-//     ifstream inputFile("../../input.txt");
-//     if (!inputFile) {
-//         cout << "Failed to open input.txt" << endl;
-//         return 1;
-//     }
-
-//     string line;
-//     map<string, double> city_costs;
-//     while (getline(inputFile, line)) {
-//         stringstream ss(line);
-//         string city, product, price;
-//         getline(ss, city, ',');
-//         getline(ss, product, ',');
-//         getline(ss, price, ',');
-//         double cost = stof(price);
-
-//         auto result = city_costs.insert({city, cost});
-//         if (!result.second) // if the insertion did not take place, the city is already in the map
-//             result.first->second += cost; // add the cost to the existing cost
-//     }
-//     map<string, double> sorted_city_costs;
-//     for (map<string, double>::iterator it = city_costs.begin(); it != city_costs.end(); it++)
-//     {
-//         sorted_city_costs.insert({it->first, it->second});
-//     }
-    
-//     for (map<string, double>::iterator it = sorted_city_costs.begin(); it != sorted_city_costs.end(); it++)
-//     {
-//         cout << it->first << ": " << fixed << it->second << endl;
-//     }
-
-//     inputFile.close();
-//     return 0;
-// }
-
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 #include <map>
 #include <vector>
 #include <queue>
@@ -73,6 +27,7 @@ int main() {
     string line;
     map<string, double> city_costs;
     map<string, double> product_costs;
+
     while (getline(inputFile, line)) {
         stringstream ss(line);
         string city, product, price;
@@ -85,8 +40,8 @@ int main() {
         if (!city_costs_insertion.second) // if the insertion did not take place, the city is already in the map
             city_costs_insertion.first->second += cost; // add the cost to the existing cost
         auto product_costs_insertion = product_costs.insert({product, cost});
-         if (!product_costs_insertion.second) // if the insertion did not take place, the city is already in the map
-            product_costs_insertion.first->second = min(product_costs_insertion.first->second, cost);
+         if (!product_costs_insertion.second) // if the insertion did not take place, the product is already in the map
+            product_costs_insertion.first->second = min(product_costs_insertion.first->second, cost); // update with the sheapest cost
     }
 
     // Create a min heap
@@ -97,25 +52,29 @@ int main() {
         sheapest_cities.push(pair);
     }
 
-    // Print the elements in ascending order of cost
-    while (!sheapest_cities.empty()) {
-        cout << sheapest_cities.top().first << ": " << fixed << sheapest_cities.top().second << endl;
-        sheapest_cities.pop();
-    }
-
-    cout << "*******************************************************" << endl;
+    cout << sheapest_cities.top().first << " " << fixed << setprecision(2) << sheapest_cities.top().second << endl; //print the sheapest city that is in the top
+    // sheapest_cities.erase(); // erase the min heap
     priority_queue<pair<string, double>, vector<pair<string, double>>, Compare> sheapest_products;
 
      // Insert all elements into the min heap
     for (auto& pair : product_costs) {
         sheapest_products.push(pair);
     }
-    // Print the elements in ascending order of cost
-    while (!sheapest_products.empty()) {
-        cout << sheapest_products.top().first << ": " << fixed << sheapest_products.top().second << endl;
+    vector<pair<string, double>> res;
+    for (size_t i = 0; i < 5 && !sheapest_products.empty(); i++) {
+        res.push_back({sheapest_products.top().first, sheapest_products.top().second});
+        // cout << sheapest_products.top().first << " " << fixed << sheapest_products.top().second << endl;
         sheapest_products.pop();
     }
+    // Print the elements in ascending order of cost
 
+    for(vector<pair<string, double>>::iterator it = res.begin(); it != res.end(); it++)
+    {
+        if (it->second == (it+1)->second)
+            iter_swap(it, it+1);
+        cout << it->first << " " << fixed << setprecision(2) << it->second << endl;
+    }
+    
     inputFile.close();
     return 0;
 }
