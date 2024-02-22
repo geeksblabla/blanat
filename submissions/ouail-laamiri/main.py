edade83d2b32799635687e827849669d4687791a
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2024-02-22 12:53:23 +0100

    first solution

--- submissions/ouail-laamiri/main.py
+++ submissions/ouail-laamiri/main.py
@@ -23,8 +23,22 @@ df = spark.read.csv("input.txt", schema=schema)
 # Group by city and compute the total price of all products
 total_price_by_city = df.groupBy("city").sum("price")
 
-# Show the total price of all products by city
-total_price_by_city.show()
+# Find the city with the lowest total price
+city_with_lowest_price = total_price_by_city.orderBy("sum(price)").first()["city"]
+
+# Filter the DataFrame to include only records for the city with the lowest total price
+filtered_df = df.filter(df["city"] == city_with_lowest_price)
+
+# Retrieve the total price for the city with the lowest total product price
+total_price_in_lowest_city = total_price_by_city.filter(total_price_by_city["city"] == city_with_lowest_price).collect()[0][1]
+
+# Order the filtered DataFrame by price in ascending order and select the first five rows
+five_least_expensive_products = filtered_df.orderBy("price").limit(5)
+
+# Show the five least expensive products in the city with the lowest total price
+print("Five least expensive products in", city_with_lowest_price , "total price : ",total_price_in_lowest_city)
+five_least_expensive_products.show()
+
 
 # Record the end time
 end_time = time.time()
