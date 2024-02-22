3021fef752fc2b0d8c5d46de4eb8b0443ff5f61f
elmoulaouianouar <elmoulaoui.anouar@gmail.com>
2024-02-22 23:51:56 +0100

    fix loads in memory

--- submissions/ElMoulaouiAnouar/index.js
+++ submissions/ElMoulaouiAnouar/index.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
+const readline = require('readline');
 
-const data = fs.readFileSync('input.txt',"utf-8");
+const fileStream = fs.createReadStream('input.txt');
 const fileStreamOut = fs.createWriteStream('output.txt');
+
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
 
 let cityWithTotal = new Map();
 let cityWithProduct = new Map();
 
-data.split('\r\n').map(line => {
+rl.on('line', (line) => {
 
    const [city , product , price] = line.split(',');
       const productwithprice = {}
@@ -16,14 +22,19 @@ data.split('\r\n').map(line => {
       cityWithTotal.set(city,(parseFloat(cityWithTotal.get(city)) + parseFloat(price) || parseFloat(price)));
   
 })
-//sort results and get first city
-const firstCityHasSmallPrice = Array.from(cityWithTotal.entries()).sort((a,b) => a[1]-b[1])[0];
 
-//write reulsts in file  
-fileStreamOut.write(`${firstCityHasSmallPrice[0]}  ${firstCityHasSmallPrice[1]} \n`)
+rl.on('close',() => {
+   //sort results and get first city
+   const firstCityHasSmallPrice = Array.from(cityWithTotal.entries()).sort((a,b) => a[1]-b[1])[1];
 
-cityWithProduct.get(firstCityHasSmallPrice[0].trim()).sort((a,b) => {
-   return parseFloat(Object.values(a)[0]) !== parseFloat(Object.values(b)[0]) ?
-         parseFloat(Object.values(a)[0]) - parseFloat(Object.values(b)[0]) 
-        : Object.keys(a)[0].localeCompare(Object.keys(b)[0])
-}).slice(0,5).map(product => fileStreamOut.write(`${Object.keys(product)[0]} ${Object.values(product)[0]} \n`))
+   //write reulsts in file  
+   fileStreamOut.write(`${firstCityHasSmallPrice[0]}  ${firstCityHasSmallPrice[1]} \n`)
+
+   cityWithProduct.get(firstCityHasSmallPrice[0].trim()).sort((a,b) => {
+      return parseFloat(Object.values(a)[0]) !== parseFloat(Object.values(b)[0]) ?
+            parseFloat(Object.values(a)[0]) - parseFloat(Object.values(b)[0]) 
+         : Object.keys(a)[0].localeCompare(Object.keys(b)[0])
+   }).slice(0,5)
+         .map(product => fileStreamOut.write(`${Object.keys(product)[0]} ${Object.values(product)[0]} \n`))
+
+})
