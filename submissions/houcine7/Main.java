706713a7ec12d0c4013f095b88644cfe0d96753a
Lahoucine EL ADDALI <92301230+houcine7@users.noreply.github.com>
2024-02-24 21:48:02 +0100

    fix: houcine7 submission; update class name & comparator fix (#152)

--- submissions/houcine7/Main.java
+++ submissions/houcine7/Main.java
@@ -7,32 +7,21 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 
-public class Blanat {
-
+public class Main {
     public class Price {
         double total;
-        int length;
         Double min ;
 
         Price(double total, int length) {
             this.total= total;
-            this.length = length;
             this.min = Double.MAX_VALUE;
         }
 
          void increaseTotal(double price) {
             total+= price;
-            length++;
             this.min = Math.min(min, price);
         }
 
-         void decreaseTotal(double price) {
-            total-= price;
-            length--;
-        }
-        public int getLength() {
-            return length;
-        }
         public double getTotal() {
             return total;
         }
@@ -53,12 +42,12 @@ public class Blanat {
                     if(cityProducts.containsKey(st[1])) {
                         cityProducts.get(st[1]).increaseTotal(Double.parseDouble(st[2]));
                     }else{
-                        Price p = new Blanat().new Price(Double.parseDouble(st[2]), 1);
+                        Price p = new Main().new Price(Double.parseDouble(st[2]), 1);
                         cityProducts.put(st[1], p);
                     }
                 }else {
                     Map<String,Price> tmp = new HashMap<>();
-                    Price p = new Blanat().new Price(Double.parseDouble(st[2]), 1);
+                    Price p = new Main().new Price(Double.parseDouble(st[2]), 1);
 
                     tmp.put(st[1], p);
                     mp.put(st[0],  tmp );
@@ -91,16 +80,16 @@ public class Blanat {
             bw.write(city+" "+min);
 
             Map<String,Price> cityProduct = mp.get(city);
-
-            bw.append("\n");
-
+            
             PriorityQueue<String> pq = new PriorityQueue<>((b,a)->{
-                if(Double.parseDouble(b.split(":")[1])-Double.parseDouble(a.split(":")[1])>0.0) {
+                String[] splitedB = b.split(":");
+                String[] splittedA = a.split(":");
+                if(Double.parseDouble(splitedB[1])-Double.parseDouble(splittedA[1])>0.0) {
                     return 1;
-                }else if(Double.parseDouble(b.split(":")[1])-Double.parseDouble(a.split(":")[1])<0.0){
+                }else if(Double.parseDouble(splitedB[1])-Double.parseDouble(splittedA[1])<0.0){
                     return -1;
                 }else{
-                    return 0;
+                    return splitedB[0].compareToIgnoreCase(splittedA[0]);
                 }
             });
 
@@ -108,25 +97,16 @@ public class Blanat {
                 pq.add(item.getKey()+":"+item.getValue().getMin());
             }
 
-            String[] tmp = new String[5];
             for(int i=0;i<5;i++) {
                 String row = pq.poll();
-                tmp[i] = row.split(":")[0]+" "+row.split(":")[1];
+                String temp= row.split(":")[0]+" "+row.split(":")[1];
+                bw.append("\n" + temp);
             }
 
-            int j=0;
-            for(String str: tmp) {
-                bw.append(str);
-                if(j!=4) bw.append("\n");
-                j++;
-            }
             bf.close();
             bw.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }finally {
-            
-        }     
-    }
+        }    }
 }
