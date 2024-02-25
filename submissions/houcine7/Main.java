d8d967c2c39322cecf19847f71b5e835901b6693
Lahoucine EL ADDALI <92301230+houcine7@users.noreply.github.com>
2024-02-25 16:51:04 +0100

    fix: houcine7 submission; format double; fix min price (#163)

--- submissions/houcine7/Main.java
+++ submissions/houcine7/Main.java
@@ -10,22 +10,22 @@ import java.util.PriorityQueue;
 public class Main {
     public class Price {
         double total;
-        Double min ;
+        double min ;
 
-        Price(double total, int length) {
+        Price(double total) {
             this.total= total;
-            this.min = Double.MAX_VALUE;
+            this.min =total;
         }
 
          void increaseTotal(double price) {
             total+= price;
-            this.min = Math.min(min, price);
+            this.min = Math.min(price, this.min);
         }
 
         public double getTotal() {
             return total;
         }
-        public Double getMin() {
+        public double getMin() {
             return min;
         }
     }
@@ -42,12 +42,12 @@ public class Main {
                     if(cityProducts.containsKey(st[1])) {
                         cityProducts.get(st[1]).increaseTotal(Double.parseDouble(st[2]));
                     }else{
-                        Price p = new Main().new Price(Double.parseDouble(st[2]), 1);
+                        Price p = new Main().new Price(Double.parseDouble(st[2]));
                         cityProducts.put(st[1], p);
                     }
                 }else {
                     Map<String,Price> tmp = new HashMap<>();
-                    Price p = new Main().new Price(Double.parseDouble(st[2]), 1);
+                    Price p = new Main().new Price(Double.parseDouble(st[2]));
 
                     tmp.put(st[1], p);
                     mp.put(st[0],  tmp );
@@ -60,8 +60,7 @@ public class Main {
             for (Map.Entry<String,Map<String,Price>> item:  mp.entrySet()) {
                for (Map.Entry<String,Price> pr : item.getValue().entrySet()) {
                     result.put(item.getKey(), 
-                    (pr.getValue().getTotal()) +
-                     result.getOrDefault(item.getKey(), 0.0)
+                     pr.getValue().getTotal() + result.getOrDefault(item.getKey(), 0.0)
                     );
                }
             }
@@ -77,7 +76,7 @@ public class Main {
 
             BufferedWriter bw = new BufferedWriter(new FileWriter("./output.txt"));
 
-            bw.write(city+" "+min);
+            bw.write(city+" "+String.format("%.2f",min));
 
             Map<String,Price> cityProduct = mp.get(city);
             
@@ -94,7 +93,7 @@ public class Main {
             });
 
             for (Map.Entry<String,Price> item :  cityProduct.entrySet()) {
-                pq.add(item.getKey()+":"+item.getValue().getMin());
+                pq.add(item.getKey()+":"+String.format("%.2f", item.getValue().getMin()));
                 if(pq.size() > 5) pq.poll();
             }
 
