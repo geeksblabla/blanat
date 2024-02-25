89a66490447ca61da1cfc6487e313ce279bed4b7
Ismail Tlemcani <ismail.tlemcani.91@gmail.com>
2024-02-25 20:50:12 +0100

    challenge: remove second loop and improve solution (#170)

--- submissions/ismailtlem/gblabla_coding_challenge.py
+++ submissions/ismailtlem/gblabla_coding_challenge.py
@@ -1,55 +1,41 @@
 from collections import defaultdict
 
 
-def get_city_totals(lines):
-    """Calculate total price for each city"""
-
-    city_totals = defaultdict(float)
-    for line in lines:
-        city, _, price = line.strip().split(",")
-        city_totals[city] += float(price)
-    return city_totals
-
-
-def read_large_file(file_path, chunk_size=256 * 256):
+def read_large_file(file_path, chunk_size=1024 * 1024):
     """Read input file by chunks"""
 
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         while True:
-            data = file.readlines(chunk_size)
-            if not data:
+            chunk = file.readline(chunk_size)
+            if not chunk:
                 break
-            yield data
+            line = chunk.strip()
+            yield line
 
 
 def main(input_path, output_path):
     """Main function"""
 
     city_totals = defaultdict(float)
-    cheapest_city_products = defaultdict(list)
-    for chunk in read_large_file(input_path):
-        for line in chunk:
-            city, _, price = line.strip().split(",")
-            city_totals[city] += float(price)
-            _, product, price = line.strip().split(",")
+    cheapest_city_products = defaultdict(set)
+    cheapest_city = ""
+    cheapest_total = float("inf")
 
-    cheapest_city = min(city_totals, key=city_totals.get)
-    cheapest_total = city_totals[cheapest_city]
-    for chunk in read_large_file(input_path):
-        for line in chunk:
-            if line.startswith(cheapest_city):
-                _, product, price = line.strip().split(",")
-                cheapest_city_products[cheapest_city].append(
-                    {"product": product, "price": float(price)}
-                )
+    for line in read_large_file(input_path):
+        city, product, price = line.split(",")
+        city_totals[city] += float(price)
+        if city_totals[city] < cheapest_total:
+            cheapest_total = city_totals[city]
+            cheapest_city = city
+            cheapest_city_products.clear()
+            cheapest_city_products.update({cheapest_city: set()})
+        if city == cheapest_city:
+            cheapest_city_products[cheapest_city].add((product, price))
     top_products = sorted(
-        cheapest_city_products[cheapest_city], key=lambda x: (x["price"], x["product"])
+        cheapest_city_products[cheapest_city], key=lambda x: (x[1], x[0])
     )[:5]
-
     output_lines = [f"{cheapest_city} {round(cheapest_total, 2)}"]
-    output_lines.extend(
-        [f"{product['product']} {product['price']}" for product in top_products]
-    )
+    output_lines.extend([f"{product[0]} {product[1]}" for product in top_products])
 
     with open(output_path, "w", encoding="utf-8") as f:
         f.write("\n".join(output_lines))
