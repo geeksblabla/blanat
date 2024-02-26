cd07bfb42c47a9720bae4b73db5c364766cc2fb8
abdallah <abdallahradfi@gmail.com>
2024-02-26 21:04:40 +0100

    commit

--- submissions/AbdallahRdf/Main.java
+++ submissions/AbdallahRdf/Main.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
