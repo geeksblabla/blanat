76d1a1f5de5af06ec0d78a91e6a028ccef828fb5
Tajeddine Bourhim <bourhimtajeddine@gmail.com>
2024-02-27 17:02:16 +0100

    Update main.c

--- submissions/scorpionTaj/main.c
+++ submissions/scorpionTaj/main.c
@@ -7,6 +7,10 @@
 #define NUM_CITIES 101
 #define NUM_PRODUCTS 94
 #define MAX_LINE_LENGTH 100
+#define MAX_PRODUCT_NAME_LENGTH 100
+#define MAX_PRODUCTS 5
+
+char printed_product_names[MAX_PRODUCTS][MAX_PRODUCT_NAME_LENGTH] = {0};
 
 const char *moroccan_cities[NUM_CITIES] = {
     "Assa",
@@ -18,6 +22,7 @@ const char *moroccan_cities[NUM_CITIES] = {
     "Fes",
     "Layoune",
     "Casablanca",
+    "casa",
     "Ben_guerir",
     "Skhirate",
     "Midelt",
@@ -42,6 +47,7 @@ const char *moroccan_cities[NUM_CITIES] = {
     "Mohammedia",
     "Nador",
     "Rabat",
+    "rabat",
     "Demnate",
     "Boulemane",
     "Ksar_El_Kebir",
@@ -114,6 +120,9 @@ const char *moroccan_cities[NUM_CITIES] = {
 
 const char *fruits_and_vegetables[NUM_PRODUCTS] = {
     "Coconut",
+    "Gaz",
+    "Sugar",
+    "Flour",
     "Cherry",
     "Pomegranate",
     "Lime",
@@ -138,6 +147,7 @@ const char *fruits_and_vegetables[NUM_PRODUCTS] = {
     "Potato",
     "Peas",
     "Parsnip",
+    "Oil",
     "Cranberry",
     "Lemon",
     "Collard_Greens",
@@ -206,88 +216,108 @@ const char *fruits_and_vegetables[NUM_PRODUCTS] = {
     "Beet",
     "Currant",
     "Sweet_Potato",
-    "Cucumber"};
+    "Cucumber"
+};
 
-#
+struct ProductPrice {
+    const char *name;
+    float price;
+};
 
-int main()
-{
+struct CityData {
+    int city_index;
+    int64_t total_price;
+    struct ProductPrice cheapest_products[5];
+};
+
+int main() {
     FILE *input_file = fopen("input.txt", "r");
     FILE *output_file = fopen("output.txt", "w");
 
-    int64_t total_per_city[NUM_CITIES];
-    memset(total_per_city, 0, sizeof(total_per_city));
+    int64_t total_per_city[NUM_CITIES] = {0};
+    struct CityData cities[NUM_CITIES] = {{0}};
 
     char line[MAX_LINE_LENGTH];
     char city[100], product[100];
     float price;
 
     // Read prices from the input file
-    while (fgets(line, sizeof(line), input_file))
-    {
-        sscanf(line, "%[^,],%[^,],%f", city, product, &price);
+    while (fgets(line, sizeof(line), input_file)) {
+        if (sscanf(line, "%[^,],%[^,],%f", city, product, &price) == 3) {
+            int city_idx = -1;
 
-        int city_idx = -1;
-
-        // Find city index
-        for (int i = 0; i < NUM_CITIES; ++i)
-        {
-            if (strcmp(moroccan_cities[i], city) == 0)
-            {
-                city_idx = i;
-                break;
-            }
-        }
-
-        if (city_idx != -1)
-        {
-            int64_t price_in_cents = price * 100;
-            total_per_city[city_idx] += price_in_cents;
-        }
-    }
-
-    // Find the 5 cheapest cities along with their total prices
-    struct CityPrice
-    {
-        int city_index;
-        int64_t total_price;
-    };
-
-    struct CityPrice cheapest_cities[5];
-    for (int i = 0; i < 5; ++i)
-    {
-        cheapest_cities[i].city_index = -1;
-        cheapest_cities[i].total_price = INT64_MAX;
-    }
-
-    for (int i = 0; i < NUM_CITIES; ++i)
-    {
-        if (total_per_city[i] > 0)
-        {
-            // Update the cheapest cities list
-            for (int j = 0; j < 5; ++j)
-            {
-                if (total_per_city[i] < cheapest_cities[j].total_price)
-                {
-                    for (int k = 4; k > j; --k)
-                    {
-                        cheapest_cities[k] = cheapest_cities[k - 1];
-                    }
-                    cheapest_cities[j].city_index = i;
-                    cheapest_cities[j].total_price = total_per_city[i];
+            // Find city index
+            for (int i = 0; i < NUM_CITIES; ++i) {
+                if (strcmp(moroccan_cities[i], city) == 0) {
+                    city_idx = i;
                     break;
                 }
             }
+
+            if (city_idx != -1) {
+                int64_t price_in_cents = price * 100;
+                total_per_city[city_idx] += price_in_cents;
+
+                // Find the index of the product
+                int product_idx = -1;
+                for (int i = 0; i < NUM_PRODUCTS; ++i) {
+                    if (strcmp(fruits_and_vegetables[i], product) == 0) {
+                        product_idx = i;
+                        break;
+                    }
+                }
+
+                // Update cheapest products for the city
+                struct ProductPrice *cheapest = cities[city_idx].cheapest_products;
+                for (int i = 0; i < 5; ++i) {
+                    if (cheapest[i].price == 0 || price_in_cents < cheapest[i].price) {
+                        // Shift cheaper products down
+                        for (int j = 4; j > i; --j) {
+                            cheapest[j] = cheapest[j - 1];
+                        }
+                        cheapest[i].name = fruits_and_vegetables[product_idx];
+                        cheapest[i].price = price_in_cents;
+                        break;
+                    }
+                }
+            } else {
+                fprintf(stderr, "Unknown city: %s\n", city);
+            }
+        } else {
+            fprintf(stderr, "Error parsing line: %s\n", line);
         }
     }
 
-    // Print the 5 cheapest cities and their total prices
-    for (int i = 0; i < 5; ++i)
-    {
-        if (cheapest_cities[i].city_index != -1)
-        {
-            fprintf(output_file, "%s %.2f\n", moroccan_cities[cheapest_cities[i].city_index], (double)cheapest_cities[i].total_price / 100.0);
+    // Find the cheapest city
+    int cheapest_city_idx = -1;
+    int64_t cheapest_city_price = INT64_MAX;
+    for (int i = 0; i < NUM_CITIES; ++i) {
+        if (total_per_city[i] > 0 && total_per_city[i] < cheapest_city_price) {
+            cheapest_city_price = total_per_city[i];
+            cheapest_city_idx = i;
         }
+    }
+
+    // Print the cheapest city and its 5 cheapest products
+    fprintf(output_file, "%s %.2f\n", moroccan_cities[cheapest_city_idx], (double)cheapest_city_price / 100.0);
+    struct ProductPrice *cheapest = cities[cheapest_city_idx].cheapest_products;
+    int printed_products = 0;
+    for (int i = 0; i < MAX_PRODUCTS; ++i) {
+        if (cheapest[i].name != NULL) {
+            int duplicate = 0;
+            for (int j = 0; j < printed_products; ++j) {
+                if (strcmp(cheapest[i].name, printed_product_names[j]) == 0) {
+                    duplicate = 1;
+                    break;
+                }
+            }
+            if (!duplicate) {
+                fprintf(output_file, "%s %.2f\n", cheapest[i].name, (double)cheapest[i].price / 100.0);
+                strncpy(printed_product_names[printed_products], cheapest[i].name, MAX_PRODUCT_NAME_LENGTH);
+                printed_products++;
+            }
+        }
+        if (printed_products == MAX_PRODUCTS) break; // Stop if we have printed 5 products
     }
 
     fclose(input_file);
