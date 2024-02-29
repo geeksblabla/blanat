b2a3d367d96b6f80e4484b05051de2513e61def4
Bekkaoui Hamza <79656539+bekkaoui99@users.noreply.github.com>
2024-02-29 06:34:45 +0100

    challenge: Hamza Bekkaoui (#251)

--- submissions/bekkaoui99/Main.java
+++ submissions/bekkaoui99/Main.java
@@ -1,104 +1,199 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
-
-class City {
-    private final String name;
-    private final List<Main.Product> products;
-    private double totalPrice;
-
-    public City(String name, List<Main.Product> products, double totalPrice) {
-        this.name = name;
-        this.products = products;
-        this.totalPrice = totalPrice;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public List<Main.Product> products() {
-        return products;
-    }
-
-    public double totalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-}
 
 public class Main {
 
-
-    public record Product(String name , double price) {}
-
+    private static final String DATASET_URL = "input.txt";
+    private static final String OUTPUT_URL = "output.txt";
+    private static final int MAX_PRODUCTS = 5;
 
     public static void main(String[] args){
 
-        List<City> cities = new ArrayList<>();
-        final int MAX_PRODUCTS = 5;
-        String dataSet = "input.txt";
-        String output = "output.txt";
+        Map<String , Data> allData = new HashMap<>();
+        allData.put("Imzouren" , new Data());
+        allData.put("Khouribga" , new Data());
+        allData.put("Bni_Hadifa" , new Data());
+        allData.put("Ouazzane" , new Data());
+        allData.put("Fes" , new Data());
+        allData.put("Tichka" , new Data());
+        allData.put("Sidi_Kacem" , new Data());
+        allData.put("Tiflet" , new Data());
+        allData.put("Sale" , new Data());
+        allData.put("El_Jadida" , new Data());
+        allData.put("Chefchaouen" , new Data());
+        allData.put("Oujda" , new Data());
+        allData.put("Midar" , new Data());
+        allData.put("Tiznit" , new Data());
+        allData.put("Arfoud" , new Data());
+        allData.put("Azilal" , new Data());
+        allData.put("Azrou" , new Data());
+        allData.put("Tichla" , new Data());
+        allData.put("Sidi_Bennour" , new Data());
+        allData.put("Nador" , new Data());
+        allData.put("Chichaoua" , new Data());
+        allData.put("Smara" , new Data());
+        allData.put("Dakhla" , new Data());
+        allData.put("Guelta_Zemmur" , new Data());
+        allData.put("Rabat" , new Data());
+        allData.put("Tetouan" , new Data());
+        allData.put("Sidi_Ifni" , new Data());
+        allData.put("Sidi_Slimane" , new Data());
+        allData.put("Béni_Mellal" , new Data());
+        allData.put("Ouarzazate" , new Data());
+        allData.put("Ifrane" , new Data());
+        allData.put("Mohammedia" , new Data());
+        allData.put("Zemamra" , new Data());
+        allData.put("Aourir" , new Data());
+        allData.put("Ksar_es_Seghir" , new Data());
+        allData.put("Laâyoune" , new Data());
+        allData.put("Souk_Larbaa" , new Data());
+        allData.put("Ahfir", new Data());
+        allData.put("Moulay_Bousselham", new Data());
+        allData.put("Casablanca", new Data());
+        allData.put("Larache", new Data());
+        allData.put("Temara", new Data());
+        allData.put("Beni_Mellal", new Data());
+        allData.put("Boujdour", new Data());
+        allData.put("Bab_Taza", new Data());
+        allData.put("Sefrou", new Data());
+        allData.put("Tinghir", new Data());
+        allData.put("Jerada", new Data());
+        allData.put("Bab_Berred", new Data());
+        allData.put("Taroudant", new Data());
+        allData.put("Tan-Tan", new Data());
+        allData.put("Berrechid", new Data());
+        allData.put("Al_Hoceima", new Data());
+        allData.put("Boulemane", new Data());
+        allData.put("Kenitra", new Data());
+        allData.put("Bir_Lehlou", new Data());
+        allData.put("Bir_Anzerane", new Data());
+        allData.put("Demnate", new Data());
+        allData.put("Guercif", new Data());
+        allData.put("Sidi_Bouzid", new Data());
+        allData.put("Ben_guerir", new Data());
+        allData.put("Jorf_El_Melha", new Data());
+        allData.put("Akhfenir", new Data());
+        allData.put("Tafraout", new Data());
+        allData.put("Taza", new Data());
+        allData.put("Kalaat_MGouna", new Data());
+        allData.put("Asilah", new Data());
+        allData.put("Goulmima", new Data());
+        allData.put("Ksar_El_Kebir", new Data());
+        allData.put("Laayoune", new Data());
+        allData.put("Layoune", new Data());
+        allData.put("Errachidia", new Data());
+        allData.put("Saidia", new Data());
+        allData.put("Youssoufia", new Data());
+        allData.put("Oujda_Angad", new Data());
+        allData.put("Assa", new Data());
+        allData.put("Tangier", new Data());
+        allData.put("Settat", new Data());
+        allData.put("Essaouira", new Data());
+        allData.put("had_soualem", new Data());
+        allData.put("Tarfaya", new Data());
+        allData.put("Marrakech", new Data());
+        allData.put("Safi", new Data());
+        allData.put("Ait_Melloul", new Data());
+        allData.put("Figuig", new Data());
+        allData.put("Zagora", new Data());
+        allData.put("Guelmim", new Data());
+        allData.put("Fquih_Ben_Salah", new Data());
+        allData.put("Oulad_Teima", new Data());
+        allData.put("Inezgane", new Data());
+        allData.put("Skhirate", new Data());
+        allData.put("Khenifra", new Data());
+        allData.put("Khemisset", new Data());
+        allData.put("Guerguerat", new Data());
+        allData.put("Meknes", new Data());
+        allData.put("Agadir", new Data());
+        allData.put("Bouarfa", new Data());
+        allData.put("Taourirt", new Data());
+        allData.put("Midelt", new Data());
+        allData.put("Berkane", new Data());
+        allData.put("Drarga", new Data());
 
 
         // reading the dataset and collecting information
-        try (BufferedReader reader = new BufferedReader(new FileReader(dataSet))){
-            String line;
-            while ((line = reader.readLine()) != null){
+        try (Stream<String> lines = Files.lines(Paths.get(DATASET_URL))){
 
+            lines.parallel().forEach(line -> {
                 String[] city_product_price = line.split(",");
                 String getCity = city_product_price[0];
                 String getProduct = city_product_price[1];
                 double getPrice = Double.parseDouble(city_product_price[2]);
+                collectingDataV2(getCity , getProduct , getPrice , allData);
 
-                Product product = new Product(getProduct , getPrice);
-                Optional<City> isExist = cities.stream()
-                        .filter(city -> city.name().equals(getCity))
-                        .findFirst();
-
-                if(isExist.isPresent()){
-                    isExist.get().products().add(product);
-                    isExist.get().setTotalPrice( isExist.get().totalPrice() + getPrice);
-                }
-                else {
-                    City city = new City(getCity , new ArrayList<>() , getPrice );
-                    city.products().add(product);
-                    cities.add(city);
-                }
-
-            }
+            });
 
         } catch (Exception e) {
             System.out.printf(e.getMessage());
         }
 
 
-        // searching for the cheapest city in the country and sort the list of products
-        City cheapestCity = cities.stream().min(Comparator.comparing(City::totalPrice)).get();
-        cheapestCity.products().sort(Comparator.comparing(Product::price).thenComparing(Product::name));
+        // find the cheapest city in the list and sort its products based on the price
+        Map.Entry<String, Data> cheapestCity = allData.entrySet().stream()
+                .min(Comparator.comparingDouble(entry -> entry.getValue().getTotalPrice())).get();
+        cheapestCity.getValue().getProducts().sort(Comparator.comparing(Product::productPrice).thenComparing(Product::productName));
+
 
         // write the result in the output file
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
-            writer.write(cheapestCity.name() + " " + String.format("%.2f" ,cheapestCity.totalPrice()) );
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_URL))) {
+            writer.write(cheapestCity.getKey() + " " + String.format("%.2f" ,cheapestCity.getValue().getTotalPrice()) );
             for (int i = 0; i < MAX_PRODUCTS; i++) {
-                writer.write("\n" + cheapestCity.products().get(i).name() + " "
-                        + String.format("%.2f" , cheapestCity.products().get(i).price()));
+                writer.write("\n" + cheapestCity.getValue().getProducts().get(i).productName() + " "
+                        + String.format("%.2f" , cheapestCity.getValue().getProducts().get(i).productPrice()));
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
-
     }
 
+
+    static synchronized void collectingDataV2(String cityName , String productName , double productPrice , Map<String , Data> allData){
+        Product product = new Product(productName , productPrice);
+        allData.get(cityName).setProducts(product);
+        allData.get(cityName).setTotalPrice(productPrice);
+    }
+
+}
+
+class Data {
+
+    private List<Product> products;
+    private double totalPrice;
+
+    public Data() {
+        products = new ArrayList<>();
+        totalPrice = 0;
+    }
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+
+    public void setTotalPrice(double totalPrice){
+        this.totalPrice += totalPrice;
+    }
+
+    public void setProducts(Product products){
+        this.products.add(products);
+    }
+
+}
+
+record Product(String productName , double productPrice) {
 }
