c0f631d4840c021c8374b9fe9d7eac001635e12d
Ismail Tlemcani <ismail.tlemcani.91@gmail.com>
2024-02-29 09:24:33 +0100

    Challenge: Fix errors in ismailtlem solution (#260)

--- submissions/ismailtlem/gblabla_coding_challenge.py
+++ submissions/ismailtlem/gblabla_coding_challenge.py
@@ -23,37 +23,25 @@ def main(input_path, output_path):
     """Main function"""
 
     city_totals = defaultdict(float)
-    cheapest_city_products = defaultdict(set)
-    cheapest_city = ""
-    cheapest_total = float("inf")
+    city_products = defaultdict(lambda: defaultdict(float))
 
     for line in read_large_file(input_path):
         city, product, price = line.split(",")
-        city_totals[city] += float(price)
-        if city_totals[city] < cheapest_total:
-            cheapest_total = city_totals[city]
-            cheapest_city = city
-            cheapest_city_products.clear()
-            cheapest_city_products.update({cheapest_city: set()})
-        if city == cheapest_city:
-            cheapest_city_products[cheapest_city].add((product, price))
-
-    top_products = sorted(cheapest_city_products[cheapest_city], key=lambda x: (x[1], x[0]))
-    lowest_prices = {}
-
-    for product, price in top_products:
-        if product not in lowest_prices or price < lowest_prices[product]:
-            lowest_prices[product] = price
-    unique_products = [(product, price) for product, price in top_products if price == lowest_prices[product]][:5]
-
+        price = float(price)
+        city_totals[city] += price
+        if not city_products[city][product] or price < city_products[city][product]:
+            city_products[city][product] = price
+    cheapest_city = min(city_totals, key=city_totals.get)
+    cheapest_products = sorted(city_products[cheapest_city].items(), key=lambda x: (x[1], x[0]))[:5]
     output_lines = [f"{cheapest_city} {format_float_number(city_totals[cheapest_city])}"]
-    output_lines.extend([f"{product[0]} {format_float_number(float(product[1]))}" for product in unique_products])
+    output_lines.extend([f"{product} {format_float_number(price)}" for product, price in cheapest_products])
 
     with open(output_path, "w", encoding="utf-8") as f:
-        f.write("\n".join(output_lines))
+        f.write("\n".join(output_lines)+ "\n")
 
 
 if __name__ == "__main__":
+
     INPUT_FILE_PATH = "input.txt"
     OUTPUT_FILE_PATH = "output.txt"
     main(INPUT_FILE_PATH, OUTPUT_FILE_PATH)
